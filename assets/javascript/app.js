cd256d579f785d5c00680e0171943dcba8c14bca
md_dev <m.damian9@hotmail.com>
2018-06-20 01:11:55 -0700

    Added more HTML, started javascript - created questions array

---
+++ assets/javascript/app.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+
+    var quizArr= [
+        {
+            question: "Who would win in a 1v1 figh to the death?",
+            answers: {
+                a: "Superman",
+                b: "Goku",
+                c: "One-Punch Man",
+                d: "Floyd Mayweather"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "What is Bitcoin?",
+            answers: {
+                a: "Imaginary internet money",
+                b: "A pump and dump / ponzi scam",
+                c: "Video game coins",
+                d: "Decentralized currency used worldwide"
+            },
+            correctAnswer: "d"
+        },
+        {
+            question: "Where is the 2018 FIFA World Cup being held?",
+            answers: {
+                a: "Mexico",
+                b: "Russia",
+                c: "North Korea",
+                d: "United States of America"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "What is the capital of California?",
+            answers: {
+                a: "Phoenix",
+                b: "Sacramento",
+                c: "Boise",
+                d: "San Diego"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "How many inches are in 1 foot?",
+            answers: {
+                a: "14",
+                b: "15",
+                c: "12",
+                d: "11"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "What color is the sky on a nice clear day?",
+            answers: {
+                a: "Blue",
+                b: "Orange",
+                c: "Purple",
+                d: "Red"
+            },
+            correctAnswer: "a"
+        },
+        {
+            question: "If you travel 70 miles at 70 miles per hour, how long did it take you to travel those 70 miles?",
+            answers: {
+                a: "70 minutes",
+                b: "140 minutes",
+                c: "60 minutes",
+                d: "50 minutes"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Bernie Sanders is a senator from the state of:",
+            answers: {
+                a: "New York",
+                b: "Vermont",
+                c: "Alabama",
+                d: "Texas"
+            },
+            correctAnswer: "b"
+        },
+        {
+            question: "How many FIFA World Cups does Brazil have?",
+            answers: {
+                a: "3",
+                b: "6",
+                c: "2",
+                d: "5"
+            },
+            correctAnswer: "d"
+        },
+        {
+            question: "What is 4 times 4?",
+            answers: {
+                a: "17",
+                b: "23",
+                c: "16",
+                d: "22"
+            },
+            correctAnswer: "c"
+        }
+    ];
+
+    console.log("Ready");
+
+});
